fbfcfb13fa05790af1302b5560ce2baa288bd322
NullPointerFound <mestour.abdelmalek@gmail.com>
2024-04-11 09:24:56 -0400

    adding Locality

---
+++ booking-hotel-service/src/main/java/com/booking/system/hotel/service/hotel_model/hotel_domain_core/entity/Locality.java
@@ -0,0 +1,19 @@
+package com.booking.system.hotel.service.hotel_model.hotel_domain_core.entity;
+
+import com.booking.system.commons.domain.core.AbstractDomainEntity;
+import com.booking.system.hotel.service.hotel_model.hotel_domain_core.valueobject.LocalityId;
+
+public class Locality extends AbstractDomainEntity<LocalityId> {
+
+    private final String country;
+    private final String state;
+    private final String city;
+
+
+    public Locality(final LocalityId id, final String city, final String state, final String country) {
+        super(id);
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+}
