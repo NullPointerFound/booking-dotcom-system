5e42af5c3008d21e405dd87b4db25c85abdda3d9
NullPointerFound <mestour.abdelmalek@gmail.com>
2024-04-11 06:11:52 -0400

    creating commons core classes

---
+++ booking-hotel-service/src/main/java/com/booking/system/commons/domain/core/AbstractDomainEntity.java
@@ -0,0 +1,43 @@
+package com.booking.system.commons.domain.core;
+
+import lombok.experimental.SuperBuilder;
+
+import java.util.StringJoiner;
+
+@SuperBuilder
+public abstract class AbstractDomainEntity<ID extends AbstractDomainEntityId<?>> {
+
+    private ID id;
+
+    protected AbstractDomainEntity(final ID id) {this.id = id;}
+
+    public void setId(final ID id) {
+        this.id = id;
+    }
+
+    public ID getId() {
+        return this.id;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+
+        final AbstractDomainEntity<?> that = (AbstractDomainEntity<?>) o;
+
+        return this.id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", AbstractDomainEntity.class.getSimpleName() + "[", "]")
+                .add("id=" + this.id)
+                .toString();
+    }
+}
