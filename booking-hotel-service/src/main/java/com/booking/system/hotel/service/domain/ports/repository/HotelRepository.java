8b419c8975cb80bedcd1b78278a235e2006ee814
NullPointerFound <mestour.abdelmalek@gmail.com>
2024-04-15 21:10:27 -0400

    adding existsCategoryById to HotelRepository

--- booking-hotel-service/src/main/java/com/booking/system/hotel/service/domain/ports/repository/HotelRepository.java
+++ booking-hotel-service/src/main/java/com/booking/system/hotel/service/domain/ports/repository/HotelRepository.java
@@ -1,10 +1,14 @@
 package com.booking.system.hotel.service.domain.ports.repository;
 
 import com.booking.system.hotel.service.domain.domain.entity.Hotel;
+import com.booking.system.hotel.service.domain.domain.valueobject.HotelCategoryId;
 
 
 public interface HotelRepository {
 
     void register( Hotel newHotel);
+
+    boolean existsCategoryById(HotelCategoryId hotelCategoryId);
+
 
 }
