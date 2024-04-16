e5630035caa040bcb556a40d68813021bf59ad7b
NullPointerFound <mestour.abdelmalek@gmail.com>
2024-04-15 23:53:23 -0400

    improving RegisterHotelUseCaseImpl for better readability

--- booking-hotel-service/src/main/java/com/booking/system/hotel/service/domain/application_service/usecase/RegisterHotelUseCaseImpl.java
+++ booking-hotel-service/src/main/java/com/booking/system/hotel/service/domain/application_service/usecase/RegisterHotelUseCaseImpl.java
@@ -3,6 +3,7 @@ package com.booking.system.hotel.service.domain.application_service.usecase;
 import com.booking.system.commons.message.ApplicationMessage;
 import com.booking.system.hotel.service.domain.application_service.dto.RegisterHotelInput;
 import com.booking.system.hotel.service.domain.application_service.dto.RegisterHotelOutput;
+import com.booking.system.hotel.service.domain.domain.entity.Hotel;
 import com.booking.system.hotel.service.domain.domain.exception.HotelDomainException;
 import com.booking.system.hotel.service.domain.ports.mapper.HotelUseCaseMapper;
 import com.booking.system.hotel.service.domain.ports.repository.HotelRepository;
@@ -24,15 +25,24 @@ public class RegisterHotelUseCaseImpl implements RegisterHotelUseCase {
     @Override
     public RegisterHotelOutput execute(RegisterHotelInput input) {
         final var hotel = this.hotelUseCaseMapper.registerHotelInputToHotel(input);
-        if (!this.hotelRepository.existsCategoryById(hotel.getCategoryId())) {
-            throw new HotelDomainException(ApplicationMessage.HOTEL_CATEGORY_NOT_FOUND);
-        }
-        if (!this.localityRepository.existsLocalityById(hotel.getLocalityId())) {
-            throw new HotelDomainException(ApplicationMessage.HOTEL_LOCALITY_NOT_FOUND);
-        }
+        this.validateCategoryId(hotel);
+        this.validateLocality(hotel);
         hotel.initialize();
         hotel.validate();
         this.hotelRepository.register(hotel);
         return this.hotelUseCaseMapper.hotelToRegisterHotelOutput(hotel);
+    }
+
+
+    private void validateCategoryId(final Hotel hotel) {
+        if (!this.hotelRepository.existsCategoryById(hotel.getCategoryId())) {
+            throw new HotelDomainException(ApplicationMessage.HOTEL_CATEGORY_NOT_FOUND);
+        }
+    }
+
+    private void validateLocality(final Hotel hotel) {
+        if (!this.localityRepository.existsLocalityById(hotel.getLocalityId())) {
+            throw new HotelDomainException(ApplicationMessage.HOTEL_LOCALITY_NOT_FOUND);
+        }
     }
 }
