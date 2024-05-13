c40b27b83a8ed9747f121e9d24766f7e4d0cf90a
NullPointerFound <mestour.abdelmalek@gmail.com>
2024-05-13 10:54:09 -0400

    adding TODOS to BookingRoomResponseHandlerImpl

--- hotel-service/src/main/java/com/booking/system/hotel/service/domain/application_service/messaging/BookingRoomResponseHandlerImpl.java
+++ hotel-service/src/main/java/com/booking/system/hotel/service/domain/application_service/messaging/BookingRoomResponseHandlerImpl.java
@@ -1,5 +1,8 @@
 package com.booking.system.hotel.service.domain.application_service.messaging;
 
+import com.booking.system.commons.domain.core.event.BookingRoomConfirmedEvent;
+import com.booking.system.commons.domain.core.event.BookingRoomFailedEvent;
+import com.booking.system.commons.domain.core.event.BookingRoomPendingEvent;
 import com.booking.system.commons.domain.core.event.BookingRoomResponseEvent;
 import com.booking.system.hotel.service.domain.ports.api.messaging.BookingRoomResponseHandler;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +12,19 @@ public class BookingRoomResponseHandlerImpl implements BookingRoomResponseHandle
 
     @Override
     public void handle(BookingRoomResponseEvent event) {
-        log.info("Booking room Response Event Received of type Pending ={}", event.getClass());
+        switch (event) {
+            case final BookingRoomPendingEvent e -> {
+                // TODO: Publish an Event to Customer Service and Payment Service
+            }
+            case final BookingRoomConfirmedEvent e -> {
+                // TODO: Publish an Event to Customer Service and Notification service
+            }
+            case final BookingRoomFailedEvent e -> {
+                // TODO: Publish an Event to Customer Service and Notification service
+            }
+            default -> throw new IllegalStateException(
+                    "Failed on handling sub-type of BookingRoomResponseEvent: Unknown event"
+            );
+        }
     }
 }
