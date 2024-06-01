c1a86833b639df21706a1774d9601c3391318014
NullPointerFound <mestour.abdelmalek@gmail.com>
2024-06-01 11:40:57 -0400

    editing the RabbitMQConfiguration of payment service module

--- payment-service/src/main/java/com/booking/system/payment/service/application/configuration/RabbitMQConfiguration.java
+++ payment-service/src/main/java/com/booking/system/payment/service/application/configuration/RabbitMQConfiguration.java
@@ -3,9 +3,14 @@ package com.booking.system.payment.service.application.configuration;
 import com.booking.system.payment.service.application.configuration.properties.ExchangeProperties;
 import com.booking.system.payment.service.application.configuration.properties.QueueProperties;
 import com.booking.system.payment.service.application.configuration.properties.RoutingKeyProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -34,4 +39,19 @@ public class RabbitMQConfiguration {
         return new Queue(this.queueProperties.paymentConfirmation(), true);
     }
 
+    @Bean
+    public Binding paymentConfirmationBinding(
+            final DirectExchange paymentExchange,
+            final Queue paymentConfirmationQueue
+    ) {
+        return BindingBuilder.bind(paymentConfirmationQueue)
+                .to(paymentExchange)
+                .with(this.routingKeyProperties.paymentConfirmation());
+    }
+
+
+    @Bean
+    public MessageConverter jsonMessageConverter(final ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
 }
