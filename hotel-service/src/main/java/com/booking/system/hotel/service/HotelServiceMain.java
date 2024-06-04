59598d7448c66d3a5f035237bf9fa5053e7ca4f9
NullPointerFound <mestour.abdelmalek@gmail.com>
2024-06-04 16:56:05 -0400

    renaming the main class of hotel service module to follow the same naming convention

--- hotel-service/src/main/java/com/booking/system/hotel/service/HotelServiceMain.java
+++ hotel-service/src/main/java/com/booking/system/hotel/service/HotelServiceMain.java
@@ -1,17 +1,15 @@
 package com.booking.system.hotel.service;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-public class BookingHotelServiceMain {
+public class HotelServiceMain {
 
     public static void main(final String[] args) {
-        SpringApplication.run(BookingHotelServiceMain.class, args);
+        SpringApplication.run(HotelServiceMain.class, args);
     }
 
 }
