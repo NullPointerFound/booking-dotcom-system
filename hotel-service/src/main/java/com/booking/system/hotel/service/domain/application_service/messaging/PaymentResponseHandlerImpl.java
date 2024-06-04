b637313770cc6e149e68c45c02f5bdd7a7d72e35
NullPointerFound <mestour.abdelmalek@gmail.com>
2024-06-04 18:27:21 -0400

    handling the different payment events in the PaymentResponseHandlerImpl of hotel service module

--- hotel-service/src/main/java/com/booking/system/hotel/service/domain/application_service/messaging/PaymentResponseHandlerImpl.java
+++ hotel-service/src/main/java/com/booking/system/hotel/service/domain/application_service/messaging/PaymentResponseHandlerImpl.java
@@ -1,5 +1,7 @@
 package com.booking.system.hotel.service.domain.application_service.messaging;
 
+import com.booking.system.commons.domain.core.event.PaymentCompletedEvent;
+import com.booking.system.commons.domain.core.event.PaymentFailedEvent;
 import com.booking.system.commons.domain.core.event.PaymentResponseEvent;
 import com.booking.system.hotel.service.domain.ports.api.messaging.PaymentResponseHandler;
 import lombok.extern.slf4j.Slf4j;
@@ -10,8 +12,26 @@ public class PaymentResponseHandlerImpl implements PaymentResponseHandler {
     @Override
     public void handle(PaymentResponseEvent event) {
 
-        log.info("Event of Type {} received", event.getClass());
+        //TODO: need to be completed
+        switch (event) {
+            case final PaymentCompletedEvent e -> {
+                log.info(
+                        "Payment completed, notifying customer service & booking service | reservationOrderId={}",
+                        e.getReservationOrderId()
+                );
 
-        // TODO: Missing the implementation
+            }
+            case final PaymentFailedEvent e -> {
+                log.info(
+                        "Payment failed with error messages \"{}\". Notifying customer service & booking service | reservationOrderId={}",
+                        String.join(", ", e.getFailureMessages()),
+                        e.getReservationOrderId()
+                );
+
+            }
+            default -> throw new IllegalStateException(
+                    "Failed on handling sub-type of PaymentResponseEvent: Unknown event"
+            );
+        }
     }
 }
